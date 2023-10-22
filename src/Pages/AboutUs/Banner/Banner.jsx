d355ea07098b83a75e9e0ec2da5ead8b42fdfd9c
Your Name <shahanricky86@gmail.com>
2023-10-23 00:40:15 +0600

    update responsive

--- src/Pages/AboutUs/Banner/Banner.jsx
+++ src/Pages/AboutUs/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="w-11/12 mx-auto">
             <div className="md:flex items-center">
-                <div className="w-1/3 p-20">
+                <div className="w-full md:w-1/3 md:p-20 p-10">
                     <img className="" src={nishanImage} alt="" />
                     <div className="mt-20">
                         <img src={signature} alt="" />
@@ -14,7 +14,7 @@ const Banner = () => {
                         <p>CEO, Nishan Media</p>
                     </div>
                 </div>
-                <div className="p-8 relative w-2/3">
+                <div className="p-8 relative md:w-2/3 w-full">
                     <h1 className="md:text-2xl text-primary font-bold mb-5">Pioneering Innovation in the Digital Print Industry</h1>
                     <p className="text-base md:text-xl">As a founder and CEO of a graphic design and printing house, my mission is to provide innovative and high-quality services to our customers, creativity, and collaboration to ensure that our clients receive the best possible products, services, and experiences. We are dedicated to understanding our customers’ needs and delivering solutions that meet and exceed their expectations. </p>
                     <p className="mt-5 text-base md:text-xl">
--- src/Pages/AboutUs/Mission/Mission.jsx
+++ src/Pages/AboutUs/Mission/Mission.jsx
@@ -6,23 +6,23 @@ import {FaCertificate } from 'react-icons/fa';
 const Mission = () => {
     return (
         <div className="bg-black text-white">
-            <div className="grid grid-cols-3 gap-5 items-center justify-center p-5 md:p-20">
-                <div className="p-5 md:p-10 text-center md:text-start  flex gap-1">
-                    <AiFillPieChart className='text-white text-3xl w-4/12' />
+            <div className="grid grid-cols-1 md:grid-cols-3  gap-5 items-center justify-center p-5 md:p-20">
+                <div className="p-5 md:p-10 text-start md:flex gap-1">
+                    <AiFillPieChart className='text-primary text-3xl md:w-4/12' />
                     <div>
                         <h2 className="text-2xl md:text-3xl mb-4">Vision</h2>
                         <p>To become the most trusted and reliable partner for clients in the graphic design and printing industry.</p>
                     </div>
                 </div>
-                <div className="p-5 md:p-10 text-center md:text-start flex gap-1">
-                    <BiPaperPlane className='text-white text-3xl w-4/12' />
+                <div className="p-5 md:p-10 text-start md:flex gap-1">
+                    <BiPaperPlane className='text-primary text-3xl md:w-4/12' />
                     <div>
                         <h2 className="text-2xl md:text-3xl mb-4">Mission</h2>
                         <p>To provide cost-effective, high-quality graphic design and printing solutions to our clients through our commitment to excellence and dedication to customer satisfaction.</p>
                     </div>
                 </div>
-                <div className="p-5 md:p-10 text-center md:text-start flex">
-                    <FaCertificate className='text-white text-3xl w-4/12' />
+                <div className="p-5 md:p-10 text-start md:flex">
+                    <FaCertificate className='text-primary text-3xl md:w-4/12' />
                     <div>
                         <h2 className="text-2xl md:text-3xl mb-4">Quality Service</h2>
                         <p>- Professional Design</p>
